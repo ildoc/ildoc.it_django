77949a30a43e331f6c4e63e036cb8ac9d00b8047
ildoc <filippo.giomi@gmail.com>
2015-03-26 16:39:45 +0100

    inizio get_images

---
+++ tools/get_images.py
@@ -0,0 +1,25 @@
+import os
+import urllib
+import urlparse
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+from django.conf import settings
+from blog.models import Post
+
+
+def is_absolute(url):
+    return bool(urlparse.urlparse(url).netloc)
+
+posts = Post.objects.all()
+image_folder = os.path.join(settings.BASE_DIR, "static\\images\\posts")
+
+
+for post in posts:
+    imgs = BeautifulSoup(post.content_html).findAll('img')
+    for img in imgs:
+        imgurl = img['src']
+        if is_absolute(imgurl):
+            print imgurl
+            urllib.urlretrieve(imgurl, os.path.join(image_folder, os.path.join(image_folder, imgurl.split('/')[-1])))
+            print 'salvata'
