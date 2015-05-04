5d6a7f110f06b46ea1114835030259420884014b
ildoc <filippo.giomi@gmail.com>
2015-05-04 13:35:55 +0200

    fix minori e update pacchetti pip

--- ildoc/settings.py
+++ ildoc/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-#LANGUAGE_CODE = 'it_IT'
-LANGUAGE_CODE = 'en_US'
+LANGUAGE_CODE = 'it_IT'
+#LANGUAGE_CODE = 'en_US'
 
 TIME_ZONE = 'UTC'
 
