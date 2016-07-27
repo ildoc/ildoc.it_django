1e7eda70e8b8b0fd4588158b46f3ecd3ad0b91d1
ildoc <filippo.giomi@gmail.com>
2016-07-28 00:07:44 +0200

    fix temporaneo

--- blog/views.py
+++ blog/views.py
@@ -10,29 +10,6 @@ from .models import Post, Category, Tag
 from .forms import PostForm
 
 
-def index(request):
-    post_list = Post.objects.filter(status=Post.PUBLISHED).prefetch_related('category').prefetch_related('tags').order_by('-pub_date')
-    paginator = Paginator(post_list, 10)
-
-    page = request.GET.get('page')
-    try:
-        latest_post_list = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        latest_post_list = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        latest_post_list = paginator.page(paginator.num_pages)
-
-    return render(
-        None,
-        'blog/index.html',
-        {
-            'latest_post_list': latest_post_list,
-        },
-        context_instance=RequestContext(request))
-
-
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(
