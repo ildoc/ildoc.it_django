3aa87f2628535d34b8bc8f16e747e07a80a1682b
ildoc <filippo.giomi@gmail.com>
2016-07-27 20:28:38 +0200

    upgrade fontawesome, aggiunte quotes in home

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,7 +1,8 @@
 from .models import Category, Post
 
+
 def blog_common(request):
-    categories = Category.objects.filter(pk__in = Post.objects.filter(status=Post.PUBLISHED).values('category')).order_by('title')
+    categories = Category.objects.filter(pk__in=Post.objects.filter(status=Post.PUBLISHED).values('category')).order_by('title')
     return {
-            'CATEGORIES': categories,
-            }
+        'CATEGORIES': categories,
+    }
--- quotes/models.py
+++ quotes/models.py
@@ -8,5 +8,12 @@ class Quote(BaseModel):
     author = models.CharField('Autore', max_length=200)
     url = models.CharField('Url', max_length=200, blank=True)
 
-    def __unicode__(self):
-        return self.text
+    def excerpt(self):
+        maxChar = 100
+        if len(self.text) > maxChar:
+            return self.text[:maxChar - 3] + '...'
+        else:
+            return self.text[:maxChar]
+
+    def __str__(self):
+        return self.excerpt()
