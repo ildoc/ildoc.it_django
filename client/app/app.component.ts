11b890bfdf4d9aa2b47d65709e93da674dd41310
ildoc <filippo.giomi@gmail.com>
2016-08-17 14:30:05 +0200

    recupero dei post

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from 'angular2/core';
+import {PostService} from '../services/post.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {OnInit} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    `,
+    providers: [PostService, HTTP_PROVIDERS]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    constructor(private _postService: PostService){
+
+    }
+
+    ngOnInit(){
+        this._postService.getPosts()
+            .subscribe(posts => console.log(posts));
+    }
+}
