3f9582a9e9e74877f37aa739be147cfada01d99e
ildoc <filippo.giomi@gmail.com>
2016-08-23 16:00:03 +0200

    creazione frontend con angular-cli

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SummaryPipe } from './summary.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryPipe
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
